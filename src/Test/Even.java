00035ef8103c5c33bf8e518c11500974a7ac9174
ManikhandanS <sami.mani10@gmail.com>
2019-07-02 11:43:17 +0530

    Add test

---
+++ src/Test/Even.java
@@ -0,0 +1,22 @@
+package Test;
+
+public class Even {
+
+		public static void main(String[] args)
+		{
+			int i,j=2;
+			for(i=0;i<=10;i++) 
+			{
+				if(i%2!=0) 
+				{
+					System.out.println("The Number " +i+ " is odd");
+				}
+				else
+				{
+					System.out.println("The Number " +i+ " is Even");
+				}
+			}
+		}
+	}
+
+
